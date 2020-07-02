867d263a97ed57c80236061586d438807a4cb87b
damiean garciamendez <damieante@gmail.com>
2020-07-02 14:11:13 -0400

    after demo

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 console.log('God Bless this code')
+
+// Create "calculator" function which accepts
+// two numbers and a callback function which will
+// some math (add, multiply) on those two numbers.
+
+const calculator = function (x, y, equipment) {
+    const output = equipment(x, y)
+    return output
+}
+
+function add (x, y) {
+    return x + y
+}
+
+function multiply (x, y) {
+    return x * y
+}
+
+function divide (x, y) {
+    return x / y
+}
+
+const addOutput = calculator(1, 2, add)
+const multiplyOutput = calculator(1, 2, multiply)
+const divideOutput = calculator(1, 2, divide)
+
+console.log({
+    addOutput,
+    multiplyOutput,
+    divideOutput,
+})
+
+console.assert(
+    addOutput === 3,
+    "Addition expected 3"
+)
+console.assert(
+    multiplyOutput === 2, 
+    "Multiplication expected 2"
+)
+console.assert(
+    divideOutput === 0.5, 
+    "Division expected 0.5"
+)
